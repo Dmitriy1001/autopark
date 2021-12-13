b0524744f92338cb6f984852d7fcf5fdbde69701
Dmitriy1001 <dumatkachuk@gmail.com>
2021-12-13 16:57:52 +0200

    added GET for created_at __gte,__lte

--- catalog_app/models.py
+++ catalog_app/models.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from django.db import models
 from django.core.validators import RegexValidator
 
@@ -5,11 +7,8 @@ from django.core.validators import RegexValidator
 class Driver(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateField(default=timezone.now)
     updated_at = models.DateField(auto_now=True)
-
-    class Meta:
-        ordering = ('created_at',)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
--- catalog_app/views.py
+++ catalog_app/views.py
@@ -5,8 +5,17 @@ from .models import Driver, Vehicle
 
 
 class DriverList(generics.ListAPIView):
-    queryset = Driver.objects.all()
     serializer_class = DriverSerializer
+
+    def get_queryset(self):
+        params = self.request.GET
+        if 'created_at__gte' in params:
+            valid_date = '-'.join(params['created_at__gte'].split('-')[::-1])
+            return Driver.objects.filter(created_at__gte=valid_date)
+        elif 'created_at__lte' in params:
+            valid_date = '-'.join(params['created_at__lte'].split('-')[::-1])
+            return Driver.objects.filter(created_at__lte=valid_date)
+        return Driver.objects.all()
 
 
 class DriverDetail(generics.RetrieveAPIView):

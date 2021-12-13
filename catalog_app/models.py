1f411eb2ef7a0385564f4d31175997b64ba1bd6a
Dmitriy1001 <dumatkachuk@gmail.com>
2021-12-13 15:25:54 +0200

    created models Driver, Vehicle

---
+++ catalog_app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+
+class Driver(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Vehicle(models.Model):
+    driver_id = models.ForeignKey(Driver, on_delete=models.SET_NULL, null=True)
+    make = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    plate_number = models.CharField(
+        max_length=255,
+        unique=True,
+        validators=[RegexValidator(r'[A-Z]{2} \d{4} [A-Z]{2}')]
+    )
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.plate_number} - {self.model}'
